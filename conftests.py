dc0ea64d22341814e27600aef393758cfe88229b
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2021-12-21 21:25:52 +0300

    Creating a fixture with the function of opening and closing the webbrowser in the conftest.py file

---
+++ conftests.py
@@ -0,0 +1,10 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()

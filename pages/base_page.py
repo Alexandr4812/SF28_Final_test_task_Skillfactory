fd405d94cc3b8301381bac6380bfd2b96d3b77e3
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2021-12-22 21:19:17 +0300

    create find_element method in base_page create find_elements method in base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,24 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-
+# создаем конструктор, который принимает browser — экземпляр webdriver.
+# Указываем url, который будет использоваться для открытия страницы.
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         # команда для неявного ожидания со значением по умолчанию в 10:
         self.browser.implicitly_wait(timeout)
+
+    # создаем метод find_element (ищет один элемент и возвращает его)
+    def find_element(self, locator, time=10):
+        return WebDriverWait(self.browser, time).until(EC.presence_of_element_located(locator),
+                                                      message=f"Can't find element by locator {locator}")
+
+    # создаем метод find_elements (ищет множество элементов и возвращает в виде списка)
+    def find_elements(self, locator, time=10):
+        return WebDriverWait(self.browser, time).until(EC.presence_of_all_elements_located(locator),
+                                                      message=f"Can't find elements by locator {locator}")
 
     # метод open должен открывать нужную страницу в браузере, используя метод get()
     def open(self):
---
+++ pages/main_page.py
@@ -0,0 +1,2 @@
+from .base_page import BasePage
+

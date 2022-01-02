72920060d13dd2021c72e2aa3136677f81ce5a79
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2022-01-02 13:16:36 +0300

    add tests EXP051 of EXP057

--- pages/login_dialog_box_page.py
+++
@@ -1,5 +0,0 @@
-from .base_page import BasePage
-from .locators import BasePageLocators, MainPageLocators, SearchPageLocators
-
-class LoginDialogBoxPage(BasePage):
-    pass
--- pages/registration_page.py
+++
@@ -1,7 +0,0 @@
-from .base_page import BasePage
-from .locators import BasePageLocators, MainPageLocators, SearchPageLocators
-
-url_registration_page = 'https://besttea.ru/profiles-add/'
-
-class RegistrationPage(BasePage):
-     pass
